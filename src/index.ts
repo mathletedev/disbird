220c40fb01a317f7ff6344ef081d847c11669b03
Delta Eames <mathletedev@gmail.com>
2022-02-06 12:55:05 -0800

    gave up on heroku

--- src/index.ts
+++ src/index.ts
@@ -20,37 +20,38 @@ bot.on("messageCreate", async (message) => {
 	)
 		return;
 
-	// try {
-	const res = await axios("https://www.bestrandoms.com/random-bird-generator");
-	console.log(res.data);
-	const document = new JSDOM(res.data).window.document;
+	try {
+		const res = await axios(
+			"https://www.bestrandoms.com/random-bird-generator"
+		);
+		const document = new JSDOM(res.data).window.document;
 
-	const bird = "#main .container div:nth-child(2) .content ul li";
+		const bird = "#main .container div:nth-child(2) .content ul li";
 
-	(message.channel as TextChannel).createMessage({
-		embed: {
-			title: `|| ${
-				document.querySelector(`${bird} p:nth-child(2) b span`)?.innerHTML
-			} || 🕊️`,
-			image: {
-				url: `https://www.bestrandoms.com${
-					(document.querySelector(`${bird} p img`) as HTMLImageElement).src
-				}`
+		(message.channel as TextChannel).createMessage({
+			embed: {
+				title: `|| ${
+					document.querySelector(`${bird} p:nth-child(2) b span`)?.innerHTML
+				} || 🕊️`,
+				image: {
+					url: `https://www.bestrandoms.com${
+						(document.querySelector(`${bird} p img`) as HTMLImageElement).src
+					}`
+				},
+				footer: {
+					text: "Click to reveal the name of this bird!",
+					icon_url: message.author.dynamicAvatarURL("png")
+				},
+				color: 0x0080ff
 			},
-			footer: {
-				text: "Click to reveal the name of this bird!",
-				icon_url: message.author.dynamicAvatarURL("png")
-			},
-			color: 0x0080ff
-		},
-		messageReference: { messageID: message.id }
-	});
-	// } catch (e) {
-	// 	(message.channel as TextChannel).createMessage({
-	// 		content: `❌ Unable to fetch a bird...\`\`\`${e}\`\`\``,
-	// 		messageReference: { messageID: message.id }
-	// 	});
-	// }
+			messageReference: { messageID: message.id }
+		});
+	} catch (e) {
+		(message.channel as TextChannel).createMessage({
+			content: `❌ Unable to fetch a bird...\`\`\`${e}\`\`\``,
+			messageReference: { messageID: message.id }
+		});
+	}
 });
 
 bot.connect();
